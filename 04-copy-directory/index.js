e61a9aea97f8e15d62f80109d40330f63ab23f6e
Alexander Nemtsev <mrcrazy696@gmail.com>
2022-11-02 21:30:04 +0300

    feat: copy directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+const copyDir = async () => {
+  try {
+    await fs.mkdir(path.join(__dirname, 'files-copy'));
+  } catch (error) {
+    if (error.code === 'EEXIST') {
+      try {
+        await fs.rm(path.join(__dirname, 'files-copy'), { recursive: true });
+        await fs.mkdir(path.join(__dirname, 'files-copy'));
+      } catch (error) {
+        console.log(error.message);
+      }
+    } else {
+      console.log(error.message);
+    }
+  }
+
+  try {
+    const files = await fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true });
+
+    for (const file of files) {
+      const filePath = path.join(__dirname, 'files', file.name);
+      const outPath = path.join(__dirname, 'files-copy', file.name);
+
+      fs.copyFile(filePath, outPath);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+copyDir();
