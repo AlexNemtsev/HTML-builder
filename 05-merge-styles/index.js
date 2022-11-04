0f6483666834bea6b07b2e2497a38b7e62684ecf
Alexander Nemtsev <mrcrazy696@gmail.com>
2022-11-04 12:20:58 +0300

    feat: merge styles done

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+const getCSSFiles = async () => {
+  const out = [];
+
+  try {
+    const files = await fsPromises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
+
+    for (const file of files) {
+      if (file.isFile()) {
+        if (file.name.split('.')[1] === 'css') out.push(file.name);
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  return out;
+};
+
+const writeStyles = async () => {
+  const writeStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+
+  const filesList = await getCSSFiles();
+  // let styles = '';
+
+  filesList.forEach(file => {
+    const readStream = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8');
+
+    readStream.on('data', chunk => writeStream.write(chunk));
+    // readStream.on('end', () => console.log(styles));
+  });
+
+  // writeStream.write(styles);
+  // console.log(styles);
+};
+
+writeStyles();
