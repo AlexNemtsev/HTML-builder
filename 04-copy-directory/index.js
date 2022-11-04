6038834145dfc11579c792eb3af03af0217c9231
Alexander Nemtsev <mrcrazy696@gmail.com>
2022-11-04 18:25:29 +0300

    fix: add params to func

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,14 +1,14 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const copyDir = async () => {
+const copyDir = async (fromFolder, toFolder) => {
   try {
-    await fs.mkdir(path.join(__dirname, 'files-copy'));
+    await fs.mkdir(toFolder);
   } catch (error) {
     if (error.code === 'EEXIST') {
       try {
-        await fs.rm(path.join(__dirname, 'files-copy'), { recursive: true });
-        await fs.mkdir(path.join(__dirname, 'files-copy'));
+        await fs.rm(toFolder, { recursive: true });
+        await fs.mkdir(toFolder);
       } catch (error) {
         console.log(error.message);
       }
@@ -18,11 +18,11 @@ const copyDir = async () => {
   }
 
   try {
-    const files = await fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true });
+    const files = await fs.readdir(fromFolder, { withFileTypes: true });
 
     for (const file of files) {
-      const filePath = path.join(__dirname, 'files', file.name);
-      const outPath = path.join(__dirname, 'files-copy', file.name);
+      const filePath = path.join(fromFolder, file.name);
+      const outPath = path.join(toFolder, file.name);
 
       fs.copyFile(filePath, outPath);
     }
@@ -31,4 +31,4 @@ const copyDir = async () => {
   }
 };
 
-copyDir();
+copyDir(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'));
