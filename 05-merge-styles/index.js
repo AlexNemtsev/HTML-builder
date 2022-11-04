4296c1a2a8313675dac8e82e8e8dfd8b40f06d21
Alexander Nemtsev <mrcrazy696@gmail.com>
2022-11-04 21:07:30 +0300

    fix: add params to functions

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const fsPromises = require('fs/promises');
 const path = require('path');
 
-const getCSSFiles = async () => {
+const getCSSFiles = async (stylesFolder) => {
   const out = [];
 
   try {
-    const files = await fsPromises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
+    const files = await fsPromises.readdir(stylesFolder, { withFileTypes: true });
 
     for (const file of files) {
       if (file.isFile()) {
@@ -20,16 +20,17 @@ const getCSSFiles = async () => {
   return out;
 };
 
-const writeStyles = async () => {
-  const writeStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+const writeStyles = async (stylesFolder, outputFile) => {
+  const writeStream = fs.createWriteStream(outputFile);
 
-  const filesList = await getCSSFiles();
+  const filesList = await getCSSFiles(stylesFolder);
 
   filesList.forEach(file => {
-    const readStream = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8');
+    const readStream = fs.createReadStream(path.join(stylesFolder, file), 'utf-8');
 
     readStream.on('data', chunk => writeStream.write(chunk));
+    readStream.on('end', () => writeStream.write('\n'));
   });
 };
 
-writeStyles();
+writeStyles(path.join(__dirname, 'styles'), path.join(__dirname, 'project-dist', 'bundle.css'));
