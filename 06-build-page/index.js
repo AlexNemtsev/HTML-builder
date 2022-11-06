7259c63a079cab7ecc099d2350c8dfdb54f95bdc
Alexander Nemtsev <mrcrazy696@gmail.com>
2022-11-06 18:42:53 +0300

    feat: build page done

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,19 +1,20 @@
-const fs = require('fs/promises');
+const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
 
 const assetsFolder = path.join(__dirname, 'assets');
+const stylesFolder = path.join(__dirname, 'styles');
+const componentsFolder = path.join(__dirname, 'components');
 const distFolder = path.join(__dirname, 'project-dist');
 
 const createFolder = async (folderPath, isRecursive) => {
   try {
-    await fs.mkdir(folderPath, { recursive: isRecursive });
+    await fsPromises.mkdir(folderPath, { recursive: isRecursive });
   } catch (error) {
     if (error.code === 'EEXIST') {
-      // console.log(folderPath, 'exist');
       try {
-        await fs.rm(folderPath, { recursive: true, force: true });
-        await fs.mkdir(folderPath);
-        // console.log(folderPath, 'removed');
+        await fsPromises.rm(folderPath, { recursive: true, force: true });
+        await fsPromises.mkdir(folderPath);
       } catch (error) {
         console.log(error.message);
       }
@@ -27,7 +28,7 @@ const copyDir = async (fromFolder, toFolder) => {
   await createFolder(toFolder, false);
 
   try {
-    const files = await fs.readdir(fromFolder, { withFileTypes: true });
+    const files = await fsPromises.readdir(fromFolder, { withFileTypes: true });
 
     for (const file of files) {
       const filePath = path.join(fromFolder, file.name);
@@ -36,7 +37,7 @@ const copyDir = async (fromFolder, toFolder) => {
       if (file.isDirectory()) {
         await copyDir(filePath, outPath);
       } else {
-        fs.copyFile(filePath, outPath);
+        fsPromises.copyFile(filePath, outPath);
       }
     }
   } catch (error) {
@@ -44,5 +45,75 @@ const copyDir = async (fromFolder, toFolder) => {
   }
 };
 
+const getFilesOfSameType = async (stylesFolder, type) => {
+  const out = [];
+
+  try {
+    const files = await fsPromises.readdir(stylesFolder, { withFileTypes: true });
+
+    for (const file of files) {
+      if (file.isFile()) {
+        if (file.name.split('.')[1] === type) out.push(file.name);
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  return out;
+};
+
+const mergeStyles = async (stylesFolder, outputFile) => {
+  const writeStream = fs.createWriteStream(outputFile);
+
+  const filesList = await getFilesOfSameType(stylesFolder, 'css');
+
+  filesList.forEach(file => {
+    const readStream = fs.createReadStream(path.join(stylesFolder, file), 'utf-8');
+
+    readStream.on('data', chunk => writeStream.write(chunk));
+    readStream.on('end', () => writeStream.write('\n'));
+  });
+};
+
+// const readTemplate = () => {
+//   let template = '';
+
+//   const readStream = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
+
+//   readStream.on('data', chunk => template += chunk);
+//   readStream.on('end', () => {
+//     return template;
+
+//   });
+
+// };
+
+const buildHtml = async () => {
+  let template = '';
+
+  const readStream = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
+  const components = await getFilesOfSameType(componentsFolder, 'html');
+
+  readStream.on('data', chunk => template += chunk);
+  readStream.on('end', async () => {
+
+    components.forEach(component => {
+      const tagName = component.split('.')[0];
+      let tagContent = '';
+      const readStream = fs.createReadStream(path.join(componentsFolder, component), 'utf-8');
+      const writeStream = fs.createWriteStream(path.join(distFolder, 'index.html'));
+      readStream.on('data', chunk => tagContent += chunk);
+      readStream.on('end', () => {
+        template = template.replace(`{{${tagName}}}`, tagContent);
+        writeStream.write(template);
+      });
+    });
+  });
+
+};
+
 createFolder(distFolder, true);
 copyDir(assetsFolder, path.join(distFolder, 'assets'));
+mergeStyles(stylesFolder, path.join(distFolder, 'style.css'));
+buildHtml();
